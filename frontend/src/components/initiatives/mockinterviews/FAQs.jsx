20c6afe71118a53dbd8edfdc36b650ca6ca70665
ashish-bazad <ashish.student.cse22@itbhu.ac.in>
2024-01-31 20:18:03 +0530

    faqs

--- frontend/src/components/initiatives/mockinterviews/FAQs.jsx
+++ frontend/src/components/initiatives/mockinterviews/FAQs.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const FAQs = () => {
   return (
     <>
-      
+      <div className="mockinterviewsFaqs">
+        <div className="mockinterviewsFaqsContainer">
+          <p className="mockinterviewsFaqsH">Frequently Asked Questions</p>
+        </div>
+      </div>
     </>
   )
 }
