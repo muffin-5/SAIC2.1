accf813a50ada71b5dd7ff670fd7fd0fc5654f63
ashish-bazad <ashish.student.cse22@itbhu.ac.in>
2024-01-31 20:11:09 +0530

    testimonial

--- frontend/src/components/initiatives/mockinterviews/Testimonials.jsx
+++ frontend/src/components/initiatives/mockinterviews/Testimonials.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-
+import arrowleft from '../../../assets/arrowleft.svg'
+import arrowright from '../../../assets/arrowright.svg'
 const Testimonials = () => {
   return (
     <>
       <div className="mockinterviewsTestimonials">
         <div className="mockinterviewsTestimonialsContainer">
-          
+          <p className="mockinterviewsTestimonialsH">Testimonials</p>
+          <div className="mockinterviewsTestimonialsCards">
+
+          </div>
+          <div className="mockinterviewsTestimonialsCardsController">
+            <img src={ arrowleft } alt="left arrow" />
+            <div className="mockinterviewsTestimonialsCardsControllerScrollBar"></div>
+            <img src={ arrowright } alt="right arrow" />
+          </div>
         </div>
       </div>
     </>
